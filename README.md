1162d98adbb7c3a618738287e44bcb743cbcdec7
unknown <ritsapptech@gmail.com>
2022-04-18 06:41:54 +0530

    Flutter code toshow Vimeo Player

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vimeo Player
+
+Show Vimeo Player in Flutter.
+
+## Getting Started
+
+This project covers how to show Vimeo Player using flutter in details.
+
+Watch below [Youtube](https://www.youtube.com/watch?v=3zzil4XEUBw) video for the code walkthrough,
+
+[![Vimeo PLayer Example](https://img.youtube.com/vi/3zzil4XEUBw/0.jpg)](https://www.youtube.com/watch?v=3zzil4XEUBw)
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:vimeo_player_flutter/vimeo_player_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vimeo Player Example'),
+      ),
+      body: VimeoPlayer(
+        videoId: '70591644',
+      ),
+    );
+  }
+}
